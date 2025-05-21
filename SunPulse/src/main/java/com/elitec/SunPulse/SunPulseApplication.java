67be9fec39b7f41f7b536573bcf0f5d4d0899fd1
Benyi Cordero <benyi.uriel2006@gmail.com>
2025-05-21 07:32:43 -0600

    Creacion del proyecto

---
+++ SunPulse/src/main/java/com/elitec/SunPulse/SunPulseApplication.java
@@ -0,0 +1,13 @@
+package com.elitec.SunPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunPulseApplication.class, args);
+	}
+
+}
---
+++ SunPulse/src/test/java/com/elitec/SunPulse/SunPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elitec.SunPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
